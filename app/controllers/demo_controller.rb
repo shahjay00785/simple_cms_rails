2694d37df1a2153f4a50e667da5c0a89f9790082
shahjay00785 <shahjay00785@gmail.com>
2017-07-16 17:08:06 +0530

    add url parameter

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,7 +3,7 @@ class DemoController < ApplicationController
 
   def index
     #render(:template => 'demo/hell')
-    render('hello')
+    #render('hello')
   end
 
 
@@ -12,6 +12,10 @@ class DemoController < ApplicationController
     #render('index')
     #redirect_to('http://www.google.com')
     @array = [1,2,3] #see in the view
+
+    #read parmater
+    @id = params['id'].to_i
+    @page = params[:page].to_i
   end
 
 
