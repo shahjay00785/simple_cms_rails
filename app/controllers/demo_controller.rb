2bc4e64ac19afac255e917c3729d4b7233c7997d
shahjay00785 <shahjay00785@gmail.com>
2017-07-16 16:00:43 +0530

    render pages

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,13 @@
 class DemoController < ApplicationController
   layout false
+
   def index
+    #render(:template => 'demo/hell')
+    render('hello')
   end
+
+  def hello
+    render('index')
+  end
+
 end
